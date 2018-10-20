a5bd3aa46dc23d298d6302f128b2f4dbbd07b37c
unochanel <shanksryouop@gmail.com>
2018-10-20 15:47:23 +0900

    create conflict

---
+++ PageViewControllerKit/ViewController/RootViewController/RootViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RootViewController.swift
+//  PageViewControllerKit
+//
+//  Created by 宇野凌平 on 2018/10/20.
+//  Copyright © 2018年 ryouheiuno. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    @IBOutlet private weak var pageContainerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+
+    private func configure() {
+        embedPageViewController()
+    }
+
+    private func embedPageViewController() {
+        let viewController = PageViewController.make()
+        embed(viewController, to: pageContainerView)
+    }
+}
