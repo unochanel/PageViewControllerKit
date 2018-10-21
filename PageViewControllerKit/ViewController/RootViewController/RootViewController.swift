34a92c0914b9d3d68fc36b75ad607b3f5de9c5c4
unochanel <shanksryouop@gmail.com>
2018-10-21 12:42:10 +0900

    showview

--- PageViewControllerKit/ViewController/RootViewController/RootViewController.swift
+++ PageViewControllerKit/ViewController/RootViewController/RootViewController.swift
@@ -8,34 +8,22 @@
 
 import UIKit
 
-fileprivate let titleWidth: CGFloat = 110.0
-fileprivate let indicatorViewHeight: CGFloat = 4.0
+
+fileprivate let titleCollectionViewHeight: CGFloat = 50.0
 
 final class RootViewController: UIViewController {
-    
     private let pageType: [PageType] = [.first, .second, .third, .forth, .fifth]
     @IBOutlet private weak var pageContainerView: UIView!
-    @IBOutlet private weak var collectionView: UICollectionView!
-
-    private let selectionIndicatorView: UIView = {
-        let view: UIView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: titleWidth, height: indicatorViewHeight))
-        view.backgroundColor = UIColor.blue
-        view.layer.cornerRadius = 1
-        view.clipsToBounds = true
-        return view
-    }()
-    
+    @IBOutlet private weak var pagingSegmentedControlView: UIView!
+    private var pagingSegmentedControl: PagingSegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
     
     private func configure() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
         embedPageViewController()
-        configureCollectionView()
-        configureIndicatorView()
+        configureSegmentedControl()
     }
     
     private func embedPageViewController() {
@@ -43,38 +31,21 @@ final class RootViewController: UIViewController {
         embed(viewController, to: pageContainerView)
     }
     
-    private func configureCollectionView() {
-        collectionView.register(UINib(nibName: SegmentCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: SegmentCell.reuseIdentifier)
-        collectionView.scrollsToTop = false
-    }
-    
-    private func configureIndicatorView() {
-        selectionIndicatorView.frame.origin.y = collectionView.frame.height - indicatorViewHeight
-        collectionView.addSubview(selectionIndicatorView)
+    private func configureSegmentedControl() {
+        pagingSegmentedControl = PagingSegmentedControl.make(items: pageType)
+        pagingSegmentedControlView.addSubview(pagingSegmentedControl)
+        adujustPaginsegmentedControlLayout()
     }
 }
 
-extension RootViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pageType.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SegmentCell.reuseIdentifier, for: indexPath) as! SegmentCell
-        cell.configure(item: pageType[indexPath.row])
-        return cell
+extension RootViewController {
+    private func adujustPaginsegmentedControlLayout() {
+        pagingSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        pagingSegmentedControl.bottomAnchor.constraint(equalTo: pagingSegmentedControlView.bottomAnchor, constant: 0.0).isActive = true
+        pagingSegmentedControl.leadingAnchor.constraint(equalTo: pagingSegmentedControlView.leadingAnchor, constant: 0.0).isActive = true
+        pagingSegmentedControl.trailingAnchor.constraint(equalTo: pagingSegmentedControlView.trailingAnchor, constant: 0.0).isActive = true
+        pagingSegmentedControl.heightAnchor.constraint(equalToConstant: titleCollectionViewHeight).isActive = true
     }
 }
 
-extension RootViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let label = UILabel()
-        label.text = pageType[indexPath.row].rawValue
-        label.sizeToFit()
-        return CGSize(width: 100, height: label.frame.size.height + 24)
-    }
 
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        selectionIndicatorView.frame.origin.x = CGFloat(indexPath.row * Int(titleWidth))
-    }
-}
